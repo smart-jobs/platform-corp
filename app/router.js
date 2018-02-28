f0c6398b8cba50e6a423afe05ef696e441adea8a
dygapp <dygapp@qq.com>
2018-02-28 17:24:54 +0800

    0228

--- app/router.js
+++ app/router.js
@@ -7,12 +7,12 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   // 企业注册
-  router.post('/api/member/login', controller.member.login);
-  router.post('/api/member/passwd', controller.member.passwd);
-  router.post('/api/member/create', controller.member.create);
-  router.post('/api/member/complete', controller.member.complete);
-  router.post('/api/member/bind', controller.member.bind);
-  router.post('/api/member/unbind', controller.member.unbind);
+  router.post('/api/login', controller.member.login);
+  router.post('/api/passwd', controller.member.passwd);
+  router.post('/api/register/create', controller.member.create);
+  router.post('/api/register/complete', controller.member.complete);
+  router.post('/api/account/bind', controller.member.bind);
+  router.post('/api/account/unbind', controller.member.unbind);
 
   // 企业管理
   router.post('/admin/checkPass', controller.admin.checkPass);
